42af86e5d467ba0e9bd9af0e56e1492f57ff119b
loren <lorenmichaelh@gmail.com>
2024-04-11 17:42:55 -0500

    create signin and sign up pages

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,6 @@ export default authMiddleware({});
 
 export const config = {
   matcher: [
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    "/(api|trpc)(.*)"
+    "/((?!.+\\.[\\w]+$|_next).*)","/(api|trpc)(.*)"
   ]
 };
