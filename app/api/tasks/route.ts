88894391f828923d4c379b5d65bf94d08cf78ae2
loren <lorenmichaelh@gmail.com>
2024-04-11 19:00:44 -0500

    setup prisma CRUD

---
+++ app/api/tasks/route.ts
@@ -0,0 +1,68 @@
+import prisma from "@/app/utils/connect";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request){
+  try{
+    const {userId} = auth()
+    if (!userId) {
+      return NextResponse.json({error: "Unauthorized", status: 401});
+    }
+
+    const {title, description, dueDate, completed, important, started, assignedUser} = await req.json()
+    
+    if (!title || !description || !dueDate) {
+      return NextResponse.json({error: "Missing required fields", status: 400});
+    }
+    
+    if (title.length < 3) {
+      return NextResponse.json({error: "Title must be at least 3 characters long", status: 400});
+    }
+
+    const task = await prisma.task.create({
+      data: {
+        title,
+        description, 
+        dueDate,
+        isCompleted: completed,
+        isImportant: important,
+        isStarted: started,
+        assignedUser: assignedUser,
+        userId: userId
+      }
+    })
+
+    return NextResponse.json(task);
+
+  } catch (error) {
+    console.log('ERROR CREATING TASK: ', error);
+    return NextResponse.json(({ error: "Error creating task", status: 500 }));
+  }
+}
+
+export async function GET(req: Request){
+  try{
+
+  } catch (error) {
+    console.log('ERROR GETTING TASK: ', error);
+    return NextResponse.json(({ error: "Error getting task", status: 500 }));
+  }
+}
+
+export async function UPDATE(req: Request){
+  try{
+
+  } catch (error) {
+    console.log('ERROR UPDATING TASKS: ', error);
+    return NextResponse.json(({ error: "Error updating task", status: 500 }));
+  }
+}
+
+export async function DELETE(req: Request){
+  try{
+
+  } catch (error) {
+    console.log('ERROR DELETING TASKS: ', error);
+    return NextResponse.json(({ error: "Error deleting task", status: 500 }));
+  }
+}
